44394357bca128e983bb77221b26f970b6e8e1a8
naaand0 <fe.beiza@duocuc.cl>
2023-12-12 14:24:56 -0300

    examennn
    entrega final

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+import  mapboxgl  from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+mapboxgl.accessToken = 'pk.eyJ1IjoidGhlZmF0bG9wZWlybyIsImEiOiJjbHExbTNnYXMwYjVjMnZyeWlydXBubTJ5In0.5cI8ThTxF92e0EwZbAqCMA';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.log(err));
